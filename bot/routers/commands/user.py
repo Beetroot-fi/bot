9fcc9de09ea18f13931af3fb1ee2dc0fde390ef4
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-27 00:23:55 +0500

    feat: change start cmd

--- bot/routers/commands/user.py
+++ bot/routers/commands/user.py
@@ -1,38 +1,32 @@
-from aiogram.utils.deep_linking import create_start_link, decode_payload
+from utils.ref_link import create_ref_link
+
 from aiogram.filters import CommandStart
 from aiogram import Router, types
-
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from crud import user as user_crud
 
 router = Router(name=__name__)
 
 
 @router.message(CommandStart())
-async def start_cmd(message: types.Message, session: AsyncSession):
-    user = await user_crud.get_user_by_tg_id(session, message.from_user.id)
-    if not user:
-        user = await user_crud.create_user(session, message.from_user.id)
-    try:
-        tg_id = int(decode_payload(message.text.split("/start")[1].strip()))
-        if tg_id != message.from_user.id and user.referral_id is None:
-            await user_crud.update_user(
-                session=session,
-                tg_id=message.from_user.id,
-                referral_id=tg_id,
-            )
-    except ValueError:
-        pass
-    ref_link = await create_start_link(
+async def start_cmd(message: types.Message):
+    ref_link = await create_ref_link(
         bot=message.bot,
         payload=str(message.from_user.id),
-        encode=True,
+        app_name="BeetrootFi",
     )
     await message.answer(
         text=(
             "Beetroot Finance is an automated yield farming aggregator on TON blockchain 💎"
             "\n\nFollow @BeetrootFinance"
             f"\n\nand invite your friends👇🏼\n<code>{ref_link}</code>"
+        ),
+        reply_markup=types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text="launch",
+                        web_app=types.WebAppInfo(url="https://app.beetroot.finance"),
+                    )
+                ]
+            ]
         ),
     )
