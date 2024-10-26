2ea02d775a985d4fae274a0d8448bf11927beca8
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-27 00:24:21 +0500

    feat: remove db middleware

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,9 +2,8 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode, ChatType
 from aiogram import Bot, Dispatcher
 
-from middlewares.database import DatabaseMiddleware
+from utils.log import setup_logging
 
-from core.models import session_factory
 from core.config import settings
 
 from filters import ChatTypeFilter
@@ -16,6 +15,8 @@ import logging
 
 
 async def main():
+    """Main entrypoint"""
+
     bot = Bot(
         token=settings.bot_token,
         default=DefaultBotProperties(
@@ -27,16 +28,11 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
     dp.message.filter(ChatTypeFilter(chat_types=[ChatType.PRIVATE]))
-    dp.update.middleware(DatabaseMiddleware(session_pool=session_factory))
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.WARNING,
-        format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
-        handlers=[logging.StreamHandler()],
-    )
+    setup_logging(level=logging.WARNING)
     asyncio.run(main())
