139479ff2a64af9e2ddfdfe81620062d7af004b1
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-01 00:39:09 +0500

    feat: update start cmd

--- bot/routers/commands/user.py
+++ bot/routers/commands/user.py
@@ -4,23 +4,16 @@ from aiogram import Router, types
 
 from states.user import GettingWalletAddressState
 
-from utils.ref_link import create_ref_link
-
 router = Router(name=__name__)
 
 
 @router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    ref_link = await create_ref_link(
-        bot=message.bot,
-        payload=str(message.from_user.id),
-        app_name="BeetrootFi",
-    )
     await message.answer(
         text=(
             "Beetroot Finance is an automated yield farming aggregator on TON blockchain 💎"
             "\n\nFollow @BeetrootFinance"
-            f"\n\nand invite your friends👇🏼\n<code>{ref_link}</code>"
+            f"\n\nTo get TESTNET USDT send /get"
         ),
         reply_markup=types.InlineKeyboardMarkup(
             inline_keyboard=[
